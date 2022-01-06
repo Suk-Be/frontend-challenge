fff850968f6859d191b1052bc1bf1810b028d097
Suk-Be Jang <s.jang@sokdesign.de>
2022-01-06 19:24:08 +0100

    start doing api mocks (todo)

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom/extend-expect'
+import { server } from './__mocks__/helloWorldAPIMocks/api-server'
+
+/**
+ * @function
+ * beforeAll - Establish API mocking before all tests.
+ * afterEach - Reset any request handlers that we may add during the tests,
+               so they don't affect other tests.
+   afterAll -  Clean up after the tests are finished.
+ */
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
